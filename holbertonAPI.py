10223fd8ca38d5b17bb399d96b83981aedeaf035
el-dani-cortes <danicor83@gmail.com>
2021-05-21 15:35:32 +0000

    ADDED | Model of the class to use all features that holberton API offers

---
+++ holbertonAPI.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Authentication to holberton API
+"""
+from requests import request
+
+
+class HolbertonAPI:
+    """
+    Class to request different information to holberton's API
+    """
+    
+    def auth_holberton():
+        """
+        Method to authenticate a user to holberton school API 
+        """
+        pass
+        
+
+    def get_profile():
+        """
+        Method to get my profile from holberton platform
+        """
+        pass
+
+    def get_project():
+        """
+        Method to get project from holberton platform
+        """
+        pass
+
+    def get_task():
+        """
+        Method to get a task from a project's holberton platform
+        """
+        pass
+
+    def request_correction():
+        """
+        Method to ask for correction of a task
+        """
+        pass
+
+    def get_correction_result():
+        """
+        Method to get correction result of a task
+        """
+        pass
+
+
+
